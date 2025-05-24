b79e90322eda6a2b58863993d1505f24f636478a
Said bouaziz <alphawork404@gmail.com>
2025-05-24 15:11:14 +0000

    Update styles and add GSAP

---
+++ gsap.js
@@ -0,0 +1,51 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".showcase-area .container", {
+  opacity: 0,
+  y: 60,
+  duration: 1.2,
+  ease: "power3.out",
+  scrollTrigger: {
+    trigger: ".showcase-area .container",
+    start: "top 80%",
+    toggleActions: "play none none reset",
+  },
+});
+
+gsap.from(".showcase-area .shapes svg", {
+  rotate: 360,
+  opacity: 0,
+  duration: 3,
+  ease: "power2.out",
+  scrollTrigger: {
+    trigger: ".showcase-area .shapes svg",
+    start: "top 90%",
+    toggleActions: "play none none reset",
+  },
+});
+gsap.from(".about-cta", {
+  opacity: 0,
+  y: 80,
+  duration: 1.2,
+  ease: "power3.out",
+  scrollTrigger: {
+    trigger: ".about-cta",
+    start: "top 80%",
+    toggleActions: "play none none reset",
+    // markers: true, // Uncomment for debugging
+  },
+});
+gsap.utils.toArray(".about-cta .card").forEach((card, i) => {
+  gsap.from(card, {
+    opacity: 0,
+    y: 60,
+    duration: 1,
+    delay: i * 0.2, // stagger effect
+    ease: "power3.out",
+    scrollTrigger: {
+      trigger: card,
+      start: "top 85%",
+      toggleActions: "play none none reset",
+    },
+  });
+});
