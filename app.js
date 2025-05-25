006aaa66b7b87d73e77c737f7be6e008211b43ab
Said Bouaziz <alphawork404@gmail.com>
2025-05-25 13:57:55 +0200

    fuhhf

---
+++ app.js
@@ -0,0 +1,6 @@
+// variables call
+const topBottom_btn = document.getElementById("topBottom_btn");
+
+topBottom_btn.addEventListener("click", () => {
+  document.body.classList.toggle("shrink");
+});
