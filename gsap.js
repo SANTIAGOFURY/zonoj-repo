3ab9749df44e55221d69e8ea9148395a4493efa3
Said Bouaziz <alphawork404@gmail.com>
2025-05-26 10:34:52 +0200

    eyfeuhfurhfr

--- gsap.js
+++ gsap.js
@@ -137,25 +137,33 @@ const wavePath = document.getElementById("wavePath");
 const wave1 = "M0,160 C480,360 960,-40 1440,160 L1440,320 L0,320 Z";
 const wave2 = "M0,120 C480,-40 960,360 1440,120 L1440,320 L0,320 Z";
 const wave3 = "M0,200 C480,-80 960,400 1440,200 L1440,320 L0,320 Z";
-
+const color1 = "#7c3aed"; // Purple
+const color2 = "#21c064"; // Green
+const color3 = "red"; // Orange
 wavePath.setAttribute("d", wave1); // Start with first shape
+
+wavePath.setAttribute("d", wave1);
+wavePath.setAttribute("fill", color1);
 
 const animateWave = () => {
   gsap.to(wavePath, {
     duration: 2,
     attr: { d: wave2 },
+    fill: color2,
     ease: "power1.inOut",
     onComplete: () => {
       gsap.to(wavePath, {
         duration: 2,
         attr: { d: wave3 },
+        fill: color3,
         ease: "power1.inOut",
         onComplete: () => {
           gsap.to(wavePath, {
             duration: 2,
             attr: { d: wave1 },
+            fill: color1,
             ease: "power1.inOut",
-            onComplete: animateWave, // loop
+            onComplete: animateWave,
           });
         },
       });
@@ -163,4 +171,6 @@ const animateWave = () => {
   });
 };
 
-animateWave(); // Start 
+animateWave();
+
+animateWave(); // Start
